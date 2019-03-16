63b9ba7bc28e07cb1ed30020b3266a79c4644ff6
Alex Kreskiyan <a.kreskiyan@gmail.com>
2019-03-16 19:52:01 +0300

    Add connection management

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators, Dispatch } from 'redux'
 
+import { actions as connectionActions } from '../../data/connection'
 import { actions as pairActions } from '../../data/pair'
 import { Store } from '../../store'
 
@@ -9,30 +10,38 @@ import styles from './index.module.scss'
 
 
 type Props = {
+    connection: Store['connection']
     pair: Store['pair']
+    connect(): void
+    disconnect(): void
     setPair(pair: string): void
 }
 
-const mapStateToProps = ({ pair }: Store) => ({ pair })
+const mapStateToProps = ({ connection, pair }: Store) => ({ connection, pair })
 
 const mapDispatchToProps = (dispatch: Dispatch) => bindActionCreators(
     {
+        ...connectionActions,
         ...pairActions,
     },
     dispatch,
 )
 
-export const Header = connect(mapStateToProps, mapDispatchToProps)(({ pair, setPair }: Props) => {
+export const Header = connect(mapStateToProps, mapDispatchToProps)((props: Props) => {
     console.warn('render')
 
-    const handleSetPair = (e: React.ChangeEvent<HTMLSelectElement>) => setPair(e.target.value)
+    const handleSetPair = (e: React.ChangeEvent<HTMLSelectElement>) => props.setPair(e.target.value)
 
     return (
         <div className={styles.container}>
             <select onChange={handleSetPair}>
-                {pair.available.map(value => <option key={value} value={value}>{value}</option>)}
+                {props.pair.available.map(value => <option key={value} value={value}>{value}</option>)}
             </select>
-            Selected pair: {pair.selected}
+            Selected pair: {props.pair.selected}
+            {props.connection.isConnected ? 'Connected' : 'Disconnected'}
+            <button onClick={props.connection.isConnected ? props.disconnect : props.connect}>
+                {props.connection.isConnected ? 'Disconnect' : 'Connect'}
+            </button>
         </div>
     )
 })
