ffa09967f6351d70d943e18828fea85aef9d467b
Alex Kreskiyan <a.kreskiyan@gmail.com>
2019-03-16 12:15:36 +0300

    base setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import styles from './App.module.scss'
 
-export default App;
+export const App = () => (
+  <div className={styles.container}>
+    <header className={styles.header}>
+      Learn React
+    </header>
+  </div>
+)
